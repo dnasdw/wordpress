5789c0b91b2cf8281bf7a58f9642bb82d1b6bd6d
Sun Daowen <sundaowen@gmail.com>
2018-02-09 16:30:22 +0800

    named container

--- clean.sh
+++ clean.sh
@@ -7,11 +7,34 @@ rootdir=`pwd`
 
 ./stop.sh
 
-sudo rm -rf last_log_nginx
-mv log_nginx last_log_nginx
-sudo rm -rf last_mysql
-mv mysql last_mysql
-sudo rm -rf last_wordpress
-mv wordpress last_wordpress
+if [ -d last_log_nginx ]; then
+    echo "remove last_log_nginx"
+    sudo rm -rf last_log_nginx
+fi
+
+if [ -d log_nginx ]; then
+    echo "move log_nginx to last_log_nginx"
+    mv log_nginx last_log_nginx
+fi
+
+if [ -d last_mysql ]; then
+    echo "remove last_mysql"
+    sudo rm -rf last_mysql
+fi
+
+if [ -d mysql ]; then
+    echo "move mysql to last_mysql"
+    mv mysql last_mysql
+fi
+
+if [ -d last_wordpress ]; then
+    echo "remove last_wordpress"
+    sudo rm -rf last_wordpress
+fi
+
+if [ -d wordpress ]; then
+    echo "move wordpress to last_wordpress"
+    mv wordpress last_wordpress
+fi
 
 cd "$cwdir"
--- stop.sh
+++ stop.sh
@@ -5,7 +5,22 @@ rootdir=`dirname "$0"`
 cd "$rootdir"
 rootdir=`pwd`
 
-docker stop $(docker ps -a -q)
-docker rm -v $(docker ps -a -q)
+container_id=`docker ps -a -q -f name=docker_my_wordpress`
+if [ -n "$container_id" ]; then
+    docker stop docker_my_wordpress
+    docker rm -v docker_my_wordpress
+fi
+
+container_id=`docker ps -a -q -f name=docker_my_nginx`
+if [ -n "$container_id" ]; then
+    docker stop docker_my_nginx
+    docker rm -v docker_my_nginx
+fi
+
+container_id=`docker ps -a -q -f name=docker_my_mysql`
+if [ -n "$container_id" ]; then
+    docker stop docker_my_mysql
+    docker rm -v docker_my_mysql
+fi
 
 cd "$cwdir"
